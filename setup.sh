cf67def0164e92c288069334a2032b635a3e7e69
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-25 08:56:27 +0700

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 echo "Dinh Truong xin chao anh em!"
 echo "Setting up the environment!"
-echo ""
-pip install accelerate
-pip install -U datasets
-pip install wandb
-pip install huggingface_hub
+echo "=========================="
+pip install -q -U datasets
+pip install -q -r requirements.txt
+echo "Installed packages:\ndatasets\nwandb\nhuggingface_hub\naccelerate\npeft\nbitsandbytes\n"
+echo "=========================="
 echo "Set up complete!"
