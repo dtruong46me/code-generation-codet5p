bdc75980fe917f821fcaf4dcd543aa6f1816adef
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-25 08:58:12 +0700

    fix

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ echo "Setting up the environment!"
 echo "=========================="
 pip install -q -U datasets
 pip install -q -r requirements.txt
+pip install -q -r /content/code-generation-codet5p/requirements.txt
+pip install -q -r /kaggle/working/code-generation-codet5p/requirements.txt
 echo "Installed packages:\ndatasets\nwandb\nhuggingface_hub\naccelerate\npeft\nbitsandbytes\n"
 echo "=========================="
 echo "Set up complete!"
