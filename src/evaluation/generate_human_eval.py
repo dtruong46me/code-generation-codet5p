9fc1451b0dcc2a43d9e9b3a068a0a0f45351cd9b
trung1812 <phamtrungnhom@gmail.com>
2024-05-09 17:11:42 +0700

    simple generation

---
+++ src/evaluation/generate_human_eval.py
@@ -0,0 +1,24 @@
+from human_eval.data import write_jsonl, read_problems
+import torch
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+
+checkpoint = "Salesforce/codet5p-220m"
+device = "cpu" # for GPU usage or "cpu" for CPU usage
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)
+
+def generate_one_completion(prompt):
+    inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
+    outputs = model.generate(inputs, max_length=10)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
+problems = read_problems()
+
+num_samples_per_task = 1
+samples = [
+    dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"]))
+    for task_id in problems
+    for _ in range(num_samples_per_task)
+]
+write_jsonl("samples.jsonl", samples)
