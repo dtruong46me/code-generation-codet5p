af9f340d27960a1f595f36c9db51556a97186e2f
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2024-06-13 15:40:48 +0700

    fix bug

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,39 +1,14 @@
-import logging
+
 from datasets import load_dataset, Dataset, concatenate_datasets
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-class IngestDataset:
-    def __init__(self, from_huggingface:str = "mbpp") -> None:
-        self.from_huggingface = from_huggingface
-
-    def get_data(self) -> Dataset:
-        logger.info(f"Loading data from {self.from_huggingface}")
-        
-        data = load_dataset(self.from_huggingface, trust_remote_code=True)
-        data = concatenate_datasets([
-            data["train"],
-            data["test"],
-            data["validation"],
-            data["prompt"]
-        ])
-
-        logger.info("Complete loading dataset from {self.from_huggingface}")
-        return data
     
-def ingest_data(from_huggingface: str) -> Dataset:
-    try:
-        ingest_data = IngestDataset(from_huggingface)
-        dataset = ingest_data.get_data()
 
-        return dataset
-    
-    except Exception as e:
-        logger.error(f"Error while ingesting data: {e}")
-        raise e
-    
-# if __name__=="__main__":
-#     data_path = "mbpp"
-#     dataset = ingest_data(data_path)
-#     print(dataset)
+def ingest_data(datapath:str="mbpp") -> Dataset:
+    data = load_dataset(datapath, trust_remote_code=True)
+    data = concatenate_datasets([
+        data["train"],
+        data["test"],
+        data["validation"],
+        data["prompt"]
+    ])
+
+    return data
--- src/utils.py
+++ src/utils.py
@@ -2,48 +2,39 @@ import yaml
 import os
 from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer, TrainerCallback, TrainingArguments, TrainerState, TrainerControl
 import argparse
-import logging
 import torch
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 def load_training_arguments(args):
-    if args.configpath is not None:
-        config = load_config(configpath=args.configpath)
-        training_args = Seq2SeqTrainingArguments(**config)
-    
-    else:
-        training_args = Seq2SeqTrainingArguments(
-            output_dir=args.output_dir,
-            overwrite_output_dir=args.overwrite_output_dir,
+    training_args = Seq2SeqTrainingArguments(
+        output_dir=args.output_dir,
+        overwrite_output_dir=args.overwrite_output_dir,
 
-            num_train_epochs=args.num_train_epochs,
-            per_device_train_batch_size=args.per_device_train_batch_size,
-            per_device_eval_batch_size=args.per_device_eval_batch_size,
-            gradient_accumulation_steps=args.gradient_accumulation_steps,
-            
-            learning_rate=args.learning_rate,
-            weight_decay=args.weight_decay,
+        num_train_epochs=args.num_train_epochs,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        per_device_eval_batch_size=args.per_device_eval_batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        
+        learning_rate=args.learning_rate,
+        weight_decay=args.weight_decay,
 
-            evaluation_strategy=args.evaluation_strategy,
-            save_strategy=args.save_strategy,
+        evaluation_strategy=args.evaluation_strategy,
+        save_strategy=args.save_strategy,
 
-            logging_strategy=args.logging_strategy,
-            logging_steps=args.logging_steps,
+        logging_strategy=args.logging_strategy,
+        logging_steps=args.logging_steps,
 
-            metric_for_best_model=args.metric_for_best_model,
-            save_total_limit=args.save_total_limit,
+        save_total_limit=args.save_total_limit,
+        predict_with_generate=args.predict_with_generate,
 
-            push_to_hub=args.push_to_hub,
-            report_to=args.report_to,
-            run_name=args.run_name
-        )
+        push_to_hub=args.push_to_hub,
+        report_to=args.report_to,
+        run_name=args.run_name
+    )
     return training_args
 
 
 def load_trainer(model, training_args, dataset, tokenizer):
-    # callbacks = [WandBCallback(model.tokenizer)]
     trainer = Seq2SeqTrainer(
         model=model,
         args=training_args,
@@ -55,11 +46,6 @@ def load_trainer(model, training_args, dataset, tokenizer):
     return trainer
 
 
-def load_config(configpath):
-    with open(configpath, "r") as f:
-        config = yaml.safe_load(f)
-    return config
-
 def load_tokens(token_path):
     if os.path.exists(token_path):
         with open(token_path, "r") as f:
@@ -70,14 +56,13 @@ def load_tokens(token_path):
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Fine tuning CodeT5 Model")
-    parser.add_argument("--configpath", type=str, default=None, help="Path to the config.yaml")
-    parser.add_argument("--huggingface_hub_token", type=str, default="none")
-    parser.add_argument("--wandb_token", type=str, default="none")
+    parser.add_argument("--huggingface_hub_token", type=str, default="")
+    parser.add_argument("--wandb_token", type=str, default="")
     parser.add_argument("--datapath", type=str, default="mbpp")
     parser.add_argument("--checkpoint", type=str, default="Salesforce/codet5p-220m", help="Model checkpoint to use")
     
     parser.add_argument("--output_dir", type=str, default="codet5p-220m-running", help="Output directory for fine-tuned model")
-    parser.add_argument("--overwrite_output_dir", type=bool, default=False)
+    parser.add_argument("--overwrite_output_dir", action="store_true")
     
     parser.add_argument("--num_train_epochs", type=int, default=3)
     parser.add_argument("--per_device_train_batch_size", type=int, default=2)
@@ -93,16 +78,14 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--logging_strategy", type=str, default="steps")
     parser.add_argument("--logging_steps", type=int, default=500)
     parser.add_argument("--save_total_limit", type=int, default=1)
-
-    parser.add_argument("--metric_for_best_model", type=str, default="eval_loss")
-    parser.add_argument("--load_best_model_at_end", type=bool, default=False)
+    parser.add_argument("--predict_with_generate", action="store_true")
     
-    parser.add_argument("--push_to_hub", type=bool, default=False)
+    parser.add_argument("--push_to_hub", action="store_true")
     parser.add_argument("--report_to", type=str, default="wandb")
-    parser.add_argument("--run_name", type=str, default="codet5p-220m-running")
+    parser.add_argument("--run_name", type=str, default="codet5p-220m")
 
-    parser.add_argument("--uselora", action="store_true", default=False)
-    parser.add_argument("--useqlora", action="store_true", default=False)
+    parser.add_argument("--lora", action="store_true")
+    parser.add_argument("--quantization", action="store_true")
 
     parser.add_argument("--lora_rank", type=int, default=16)
     parser.add_argument("--lora_alpha", type=int, default=32)
