863c6ec13c6973d2f6afb36eb754a85a192659a4
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-06-19 14:42:04 +0700

    fix

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,17 +1,13 @@
-import logging
+
 from datasets import load_dataset, Dataset, concatenate_datasets
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 class IngestDataset:
-    def __init__(self, from_huggingface:str = "mbpp") -> None:
-        self.from_huggingface = from_huggingface
+    def __init__(self, datapath:str = "mbpp") -> None:
+        self.datapath = datapath
 
     def get_data(self) -> Dataset:
-        logger.info(f"Loading data from {self.from_huggingface}")
         
-        data = load_dataset(self.from_huggingface, trust_remote_code=True)
+        data = load_dataset(self.datapath, trust_remote_code=True)
         data = concatenate_datasets([
             data["train"],
             data["test"],
@@ -19,21 +15,14 @@ class IngestDataset:
             data["prompt"]
         ])
 
-        logger.info("Complete loading dataset from {self.from_huggingface}")
         return data
     
-def ingest_data(from_huggingface: str) -> Dataset:
+def ingest_data(datapath: str) -> Dataset:
     try:
-        ingest_data = IngestDataset(from_huggingface)
+        ingest_data = IngestDataset(datapath)
         dataset = ingest_data.get_data()
 
         return dataset
     
     except Exception as e:
-        logger.error(f"Error while ingesting data: {e}")
         raise e
-    
-# if __name__=="__main__":
-#     data_path = "mbpp"
-#     dataset = ingest_data(data_path)
-#     print(dataset)
